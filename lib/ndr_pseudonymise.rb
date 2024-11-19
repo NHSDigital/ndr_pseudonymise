f64ac4c2546c2052ad5c28dd7a936ac347ae2596
Brian Shand <Brian.Shand@nhs.net>
2024-11-19 12:23:45 +0000

    Fix Rails 7.1 tests

--- lib/ndr_pseudonymise.rb
+++ lib/ndr_pseudonymise.rb
@@ -1,3 +1,9 @@
+require 'active_support/deprecation'
+begin
+  require 'active_support/deprecator'
+rescue LoadError
+  # Ignore error: 'active_support/deprecator' is not defined on active_support 7.0
+end
 require 'active_support'
 
 require 'ndr_pseudonymise/version'
